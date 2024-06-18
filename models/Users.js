c7b814003dfd87a32cf24866b5a6c1422e1aa3a6
Akaoni <daniel.joya@urp.edu.pe>
2024-06-17 23:37:22 -0500

    login back

---
+++ models/Users.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+    nombres:{
+        type: String,
+        required: true
+    },
+    apellidos:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    role:{
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Users', UserSchema);

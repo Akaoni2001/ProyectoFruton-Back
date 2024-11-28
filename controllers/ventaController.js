0ec51a249186eb1efef30f89671d210c37e17301
Alezzandromv <ale722sandromv35forma@gmail.com>
2024-11-28 13:42:23 -0500

    cambio

--- controllers/ventaController.js
+++ controllers/ventaController.js
@@ -7,10 +7,6 @@ exports.registrarVenta = async (req,res)=>{
         const { nombreCliente, productos, cantidades, precioTotal } = req.body;
 
         const productosDetalles = [];
-        
-        
-
-        
         for (const productoId of productos) {
             const producto = await Producto.findById(productoId);
             if (!producto) {
@@ -50,6 +46,47 @@ exports.registrarVenta = async (req,res)=>{
         res.status(500).json({ message: 'Error al registrar la venta' });
     }
 }
+
+exports.editarVenta = async (req, res) => {
+    try {
+        const { id } = req.params; 
+        const { nombreCliente, productos, cantidades, precioTotal } = req.body;
+
+        const productosDetalles = [];
+        for (const productoId of productos) {
+            const producto = await Producto.findById(productoId);
+            if (!producto) {
+                return res.status(404).json({ message: `Producto con ID ${productoId} no encontrado` });
+            }
+            productosDetalles.push(producto);
+        }
+
+        if (productosDetalles.length === 0) {
+            return res.status(400).json({ message: 'La lista de productos no puede estar vacía' });
+        }
+
+        // Buscar la venta y actualizarla
+        const ventaExistente = await Venta.findById(id);
+        if (!ventaExistente) {
+            return res.status(404).json({ message: 'Venta no encontrada' });
+        }
+
+        ventaExistente.nombreCliente = nombreCliente;
+        ventaExistente.productos = productosDetalles;
+        ventaExistente.cantidades = cantidades;
+        ventaExistente.precioTotal = precioTotal;
+        ventaExistente.fechaVenta = Date.now(); 
+
+        await ventaExistente.save();
+
+        res.status(200).json({ message: 'Venta actualizada exitosamente', venta: ventaExistente });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error al actualizar la venta' });
+    }
+};
+
+
 
 exports.obtenerVentas= async(req,res)=>{
     try {
--- routes/venta.js
+++ routes/venta.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const ventaController = require('../controllers/ventaController');
 
 router.post('/registrar',ventaController.registrarVenta);
+router.put('/:id',ventaController.editarVenta);
 router.get('/', ventaController.obtenerVentas)
 
 module.exports = router;
